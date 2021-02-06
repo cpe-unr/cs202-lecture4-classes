39c5086a1f13efe12a9b95802e23e4b6247e17d8
Keith Lancaster <keithlancaster@unr.edu>
2021-02-06 15:55:12 -0800

    initial version of OO - using a struct for methods

--- main.cpp
+++ main.cpp
@@ -5,17 +5,42 @@
 const int NUMCATPARAMS = 5;
 using namespace std;
 
-// Version 1: (better) procedural programming
-int catSleepTime(int catAge, string catBreed);
+// Version 2: Creeping toward an object-oriented approach
 
+struct Cat {
+    void getCatInfo(ifstream &catDataStream, string &catName, string &nameCatCallsSelf, int &catAge, string &catFurColor,
+                    string &catBreed, string catInfo[]) {
+        int index = 0;
+        string tmp;
+        for (index = 0; index < NUMCATPARAMS; index++) {
+            getline(catDataStream, catInfo[index]);
+        }
+        catName = catInfo[0];
+        nameCatCallsSelf = catInfo[1];
+        catAge = stoi(catInfo[2]);
+        catFurColor = catInfo[3];
+        catBreed = catInfo[4];
+    }
 
-void printCatDetails(const string &catName, const string &nameCatCallsSelf, int catAge, const string &catFurColor,
-                     const string &catBreed);
+    void printCatDetails(const string &catName, const string &nameCatCallsSelf, int catAge, const string &catFurColor,
+                         const string &catBreed) {
+        cout << "Cat Name: " << catName << "\n";
+        cout << "Cat Real Name: " << nameCatCallsSelf << "\n";
+        cout << "Cat Fur Color: " << catFurColor << "\n";
+        cout << "Cat Breed: " << catBreed << "\n";
+        int sleepTime = catSleepTime(catAge, catBreed);
+        cout << "Cat Sleep Time: " << sleepTime << "\n";
+    }
 
-// ifstream cannot be const!
-void getCatInfo(ifstream &catDataStream, string &catName, string &nameCatCallsSelf,
-                int &catAge, string &catFurColor,
-                string &catBreed, string catInfo[]);
+    int catSleepTime(int catAge, string catBreed) {
+        int sleepTime = 0;
+        if (catBreed.compare("persion")) {
+            return catAge > 10 ? 20 : 10;
+        };
+        return sleepTime;
+
+    }
+};
 
 int main() {
     ifstream catDataStream;
@@ -30,41 +55,11 @@ int main() {
     string catFurColor;
     string catBreed;
     string catInfo[NUMCATPARAMS];
-    getCatInfo(catDataStream, catName, nameCatCallsSelf, catAge,
+    Cat myCat;
+    myCat.getCatInfo(catDataStream, catName, nameCatCallsSelf, catAge,
                catFurColor, catBreed, catInfo);
-    printCatDetails(catName, nameCatCallsSelf, catAge, catFurColor, catBreed);
+    myCat.printCatDetails(catName, nameCatCallsSelf, catAge, catFurColor, catBreed);
     return 0;
 }
 
-void getCatInfo(ifstream &catDataStream, string &catName, string &nameCatCallsSelf, int &catAge, string &catFurColor,
-           string &catBreed, string catInfo[]) {
-    int index = 0;
-    string tmp;
-    for (index = 0; index < NUMCATPARAMS; index++) {
-        getline(catDataStream, catInfo[index]);
-    }
-    catName = catInfo[0];
-    nameCatCallsSelf = catInfo[1];
-    catAge = stoi(catInfo[2]);
-    catFurColor = catInfo[3];
-    catBreed = catInfo[4];
-}
 
-void printCatDetails(const string &catName, const string &nameCatCallsSelf, int catAge, const string &catFurColor,
-                     const string &catBreed) {
-    cout << "Cat Name: " << catName << "\n";
-    cout << "Cat Real Name: " << nameCatCallsSelf << "\n";
-    cout << "Cat Fur Color: " << catFurColor << "\n";
-    cout << "Cat Breed: " << catBreed << "\n";
-    int sleepTime = catSleepTime(catAge, catBreed);
-    cout << "Cat Sleep Time: " << sleepTime << "\n";
-}
-
-int catSleepTime(int catAge, string catBreed) {
-    int sleepTime = 0;
-    if (catBreed.compare("persion")) {
-        return catAge > 10 ? 20 : 10;
-    };
-    return sleepTime;
-
-}
