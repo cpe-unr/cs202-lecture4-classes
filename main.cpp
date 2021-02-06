0eca567ab457f3d4ae9502cf4fb2364f85e43223
Keith Lancaster <keithlancaster@unr.edu>
2021-02-06 15:21:04 -0800

    refactored procedural code

--- main.cpp
+++ main.cpp
@@ -5,40 +5,64 @@
 const int NUMCATPARAMS = 5;
 using namespace std;
 
-// Version 0: (bad) procedural programming
+// Version 1: (better) procedural programming
 int catSleepTime(int catAge, string catBreed);
 
+
+void printCatDetails(const string &catName, const string &nameCatCallsSelf, int catAge, const string &catFurColor,
+                     const string &catBreed);
+
+// ifstream cannot be const!
+void getCatInfo(ifstream &catDataStream, string &catName, string &nameCatCallsSelf,
+                int &catAge, string &catFurColor,
+                string &catBreed, string catInfo[]);
 
 int main() {
     ifstream catDataStream;
     catDataStream.open("catdata.txt");
-    if(catDataStream.bad()){
+    if (catDataStream.bad()) {
         cout << "Could not open file" << endl;
         return 0;
     }
+    string catName;
+    string nameCatCallsSelf;
+    int catAge;
+    string catFurColor;
+    string catBreed;
     string catInfo[NUMCATPARAMS];
+    getCatInfo(catDataStream, catName, nameCatCallsSelf, catAge,
+               catFurColor, catBreed, catInfo);
+    printCatDetails(catName, nameCatCallsSelf, catAge, catFurColor, catBreed);
+    return 0;
+}
+
+void getCatInfo(ifstream &catDataStream, string &catName, string &nameCatCallsSelf, int &catAge, string &catFurColor,
+           string &catBreed, string catInfo[]) {
     int index = 0;
     string tmp;
-    for(index = 0; index < NUMCATPARAMS; index++){
+    for (index = 0; index < NUMCATPARAMS; index++) {
         getline(catDataStream, catInfo[index]);
     }
-    string catName = catInfo[0];
-    string nameCatCallsSelf = catInfo[1];
-    int catAge =  stoi(catInfo[2]);
-    string catFurColor = catInfo[3];
-    string catBreed = catInfo[4];
+    catName = catInfo[0];
+    nameCatCallsSelf = catInfo[1];
+    catAge = stoi(catInfo[2]);
+    catFurColor = catInfo[3];
+    catBreed = catInfo[4];
+}
+
+void printCatDetails(const string &catName, const string &nameCatCallsSelf, int catAge, const string &catFurColor,
+                     const string &catBreed) {
     cout << "Cat Name: " << catName << "\n";
     cout << "Cat Real Name: " << nameCatCallsSelf << "\n";
     cout << "Cat Fur Color: " << catFurColor << "\n";
     cout << "Cat Breed: " << catBreed << "\n";
     int sleepTime = catSleepTime(catAge, catBreed);
     cout << "Cat Sleep Time: " << sleepTime << "\n";
-    return 0;
 }
 
-int catSleepTime(int catAge, string catBreed){
+int catSleepTime(int catAge, string catBreed) {
     int sleepTime = 0;
-    if(catBreed.compare("persion")){
+    if (catBreed.compare("persion")) {
         return catAge > 10 ? 20 : 10;
     };
     return sleepTime;
